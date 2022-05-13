2e704c15e2608c3145fbd7c68095668a36084206
Amjad Jibon <amjad.jibon@gmail.com>
2022-05-13 17:54:44 +0600

    github workflow added

--- registry/registry.go
+++ registry/registry.go
@@ -19,13 +19,17 @@ func (g *encodingRegistry) setup() {
 	g.registry = make(map[string]iface.IEncoding)
 }
 
+// AddEncoding adds an encoding to the registry
 func (g *encodingRegistry) AddEncoding(name string, encoding iface.IEncoding) {
 	g.registry[name] = encoding
 }
+
+// GetEncoding returns the encoding with the given name.
 func (g *encodingRegistry) GetEncoding(name string) iface.IEncoding {
 	return g.registry[name]
 }
 
+// EncodingRegistry returns the global encoding registry
 func EncodingRegistry() *encodingRegistry {
 	return encodingRegistryIns
 }
